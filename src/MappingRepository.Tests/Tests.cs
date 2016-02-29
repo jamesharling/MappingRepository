f04392dfbc889ea6b5094eae6a8412a7c33fea5a
James Harling <james@jamesharling.me>
2016-02-29 12:29:21 +0000

    Ongoing work

--- src/MappingRepository.Tests/Tests.cs
+++ src/MappingRepository.Tests/Tests.cs
@@ -2,7 +2,6 @@
 using FluentAssertions;
 using MappingRepository.Tests.Implementations.Context;
 using MappingRepository.Tests.Implementations.Repositories;
-using MappingRepository.Tests.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -18,197 +17,6 @@ namespace MappingRepository.Tests
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
             Database.SetInitializer(new DbInitializer());
-        }
-
-        [TestMethod]
-        public void CanAdd()
-        {
-            var repo = this.getRepo();
-
-            int count = repo.Count();
-
-            count.Should().Be(2);
-
-            var customer = new Implementations.DomainObjects.Customer()
-            {
-                Name = "Amanda Clement"
-            };
-
-            repo.Add(customer);
-
-            count = repo.Count();
-
-            count.Should().Be(3);
-        }
-
-        [TestMethod]
-        public void CanAddRange()
-        {
-            var repo = this.getRepo();
-
-            var customers = new List<Implementations.DomainObjects.Customer>();
-
-            var customer1 = new Implementations.DomainObjects.Customer()
-            {
-                Name = "Amanda Clement"
-            };
-
-            var customer2 = new Implementations.DomainObjects.Customer()
-            {
-                Name = "Kevin Ronalds"
-            };
-
-            customers.Add(customer1);
-            customers.Add(customer2);
-
-            repo.AddRange(customers);
-
-            var result = repo.Count();
-
-            result.Should().Be(4);
-        }
-
-        [TestMethod]
-        public void CanAny()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.Any(x => x.FirstName == "Barry");
-
-            result.Should().Be(true);
-        }
-
-        [TestMethod]
-        public void CanAsFilteredQueryable()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.FilteredQueryable();
-
-            result.Count.Should().Be(1);
-        }
-
-        [TestMethod]
-        public void CanAsQueryable()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.Queryable();
-
-            result.Count.Should().Be(2);
-        }
-
-        [TestMethod]
-        public void CanDelete()
-        {
-            var repo = this.getRepo();
-
-            var customerToDelete = repo.FirstOrDefault(x => x.FirstName.Equals("Barry"));
-
-            customerToDelete.Should().NotBeNull();
-
-            var result = repo.Delete(customerToDelete.Id);
-
-            result.Should().Be(1);
-        }
-
-        [TestMethod]
-        public void CanDeleteRange()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.DeleteRange(x => x.FirstName != null);
-
-            result.Should().Be(2);
-        }
-
-        [TestMethod]
-        public void CanEdit()
-        {
-            var repo = this.getRepo();
-
-            var customerToEdit = repo.FirstOrDefault(x => x.FirstName.Equals("Sarah"));
-
-            customerToEdit.Should().NotBeNull();
-
-            customerToEdit.Name = "Sarah Moore";
-
-            var result = repo.Edit(customerToEdit);
-
-            result.Should().Be(1);
-
-            var customer = repo.GetById(customerToEdit.Id);
-
-            customer.Name.Should().Be("Sarah Moore");
-        }
-
-        [TestMethod]
-        public void CanFindBy()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.FindBy(x => x.FirstName.Equals("Barry"));
-
-            result.Count.Should().Be(1);
-            result.Single().Name.Should().Be("Barry Jenkins");
-        }
-
-        [TestMethod]
-        public void CanFirstOrDefault()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.FirstOrDefault(x => x.FirstName.Equals("Barry"));
-
-            result.Name.Should().Be("Barry Jenkins");
-
-            result = repo.FirstOrDefault(x => x.FirstName.Equals("Jackie"));
-
-            result.Should().BeNull();
-        }
-
-        [TestMethod]
-        public void CanGetById()
-        {
-            var repo = this.getRepo();
-
-            var customerToGet = repo.FirstOrDefault(x => x.FirstName.Equals("Sarah"));
-
-            customerToGet.Should().NotBeNull();
-
-            var result = repo.GetById(customerToGet.Id);
-
-            result.Name.Should().Be("Sarah Barnes");
-        }
-
-        [TestMethod]
-        public void CanProjectTo()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.GetAllAsLite();
-
-            result.Count.Should().Be(2);
-            result.First().GetType().Should().Be(typeof(Implementations.DomainObjects.LiteCustomer));
-        }
-
-        [TestMethod]
-        public void CanSingleOrDefault()
-        {
-            var repo = this.getRepo();
-
-            var result = repo.SingleOrDefault(x => x.FirstName.Equals("Barry"));
-
-            result.Name.Should().Be("Barry Jenkins");
-
-            try
-            {
-                result = repo.SingleOrDefault(x => x.FirstName != null);
-            }
-            catch (Exception ex)
-            {
-                ex.GetType().Should().Be(typeof(InvalidOperationException));
-            }
         }
 
         private IMapper mapper
@@ -236,6 +44,250 @@ namespace MappingRepository.Tests
             }
         }
 
-        private CustomerRepository getRepo() => new CustomerRepository(new Implementations.Context.DbContext(), this.mapper);
+        private IMapper mapperThatIgnoresChildren
+        {
+            get
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AllowNullCollections = true;
+
+                    cfg.CreateMap<Implementations.Entities.Customer, Implementations.DomainObjects.Customer>()
+                        .ForMember(d => d.Orders, o => o.Ignore());
+                    cfg.CreateMap<Implementations.Entities.Customer, Implementations.DomainObjects.LiteCustomer>();
+                    cfg.CreateMap<Implementations.Entities.Order, Implementations.DomainObjects.Order>();
+
+                    cfg.CreateMap<Implementations.DomainObjects.Customer, Implementations.Entities.Customer>()
+                        .ForMember(d => d.Id, o => o.Ignore())
+                        .ForMember(d => d.FirstName, o => o.MapFrom(s => s.Name.Split(' ')[0]))
+                        .ForMember(d => d.Surname, o => o.MapFrom(s => s.Name.Split(' ')[1]))
+                        .ForMember(d => d.Orders, o => o.Ignore());
+
+                    cfg.CreateMap<Implementations.DomainObjects.Order, Implementations.Entities.Order>()
+                        .ForMember(d => d.Id, o => o.Ignore());
+                });
+
+                return config.CreateMapper();
+            }
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            var repo = this.getRepo();
+
+            int count = repo.Count();
+
+            count.Should().Be(2);
+
+            var customer = new Implementations.DomainObjects.Customer()
+            {
+                Name = "Amanda Clement"
+            };
+
+            var x = repo.Add(customer);
+
+            count = repo.Count();
+
+            count.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void AddRange()
+        {
+            var repo = this.getRepo();
+
+            var customers = new List<Implementations.DomainObjects.Customer>();
+
+            var customer1 = new Implementations.DomainObjects.Customer()
+            {
+                Name = "Amanda Clement"
+            };
+
+            var customer2 = new Implementations.DomainObjects.Customer()
+            {
+                Name = "Kevin Ronalds"
+            };
+
+            customers.Add(customer1);
+            customers.Add(customer2);
+
+            repo.AddRange(customers);
+
+            var result = repo.Count();
+
+            result.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void Any()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.Any(x => x.FirstName == "Barry");
+
+            result.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void AsFilteredQueryable()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.FilteredQueryable();
+
+            result.Count.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void AsQueryable()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.Queryable();
+
+            result.Count.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            var repo = this.getRepo();
+
+            var customerToDelete = repo.FirstOrDefault(x => x.FirstName.Equals("Barry"));
+
+            customerToDelete.Should().NotBeNull();
+
+            var result = repo.Delete(customerToDelete.Id);
+
+            result.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void DeleteRange()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.DeleteRange(x => x.FirstName != null);
+
+            result.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            var repo = this.getRepo(this.mapperThatIgnoresChildren);
+
+            var customerToEdit = repo.FirstOrDefault(x => x.FirstName.Equals("Sarah"));
+
+            customerToEdit.Should().NotBeNull();
+
+            customerToEdit.Name = "Sarah Moore";
+
+            var result = repo.Edit(customerToEdit);
+
+            result.Should().Be(1);
+
+            var customer = repo.GetById(customerToEdit.Id);
+
+            customer.Name.Should().Be("Sarah Moore");
+        }
+
+        [TestMethod]
+        public void EditWithChildObjects()
+        {
+            var repo = this.getRepo();
+
+            var customerToEdit = repo.FirstOrDefault(x => x.FirstName.Equals("Sarah"));
+
+            customerToEdit.Should().NotBeNull();
+
+            customerToEdit.Name = "Sarah Moore";
+            customerToEdit.Orders = new List<Implementations.DomainObjects.Order>()
+            {
+                new Implementations.DomainObjects.Order() { Number = 39342, Item = "Macbeth" }
+            };
+
+            var result = repo.Edit(customerToEdit);
+
+            result.Should().Be(2);
+
+            var customer = repo.GetById(customerToEdit.Id);
+
+            customer.Name.Should().Be("Sarah Moore");
+        }
+
+        [TestMethod]
+        public void FindBy()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.FindBy(x => x.FirstName.Equals("Barry"));
+
+            result.Count.Should().Be(1);
+            result.Single().Name.Should().Be("Barry Jenkins");
+        }
+
+        [TestMethod]
+        public void FirstOrDefault()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.FirstOrDefault(x => x.FirstName.Equals("Barry"));
+
+            result.Name.Should().Be("Barry Jenkins");
+
+            result = repo.FirstOrDefault(x => x.FirstName.Equals("Jackie"));
+
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            var repo = this.getRepo();
+
+            var customerToGet = repo.FirstOrDefault(x => x.FirstName.Equals("Sarah"));
+
+            customerToGet.Should().NotBeNull();
+
+            var result = repo.GetById(customerToGet.Id);
+
+            result.Name.Should().Be("Sarah Barnes");
+        }
+
+        [TestMethod]
+        public void ProjectTo()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.GetAllAsLite();
+
+            result.Count.Should().Be(2);
+            result.First().GetType().Should().Be(typeof(Implementations.DomainObjects.LiteCustomer));
+        }
+
+        [TestMethod]
+        public void SingleOrDefault()
+        {
+            var repo = this.getRepo();
+
+            var result = repo.SingleOrDefault(x => x.FirstName.Equals("Barry"));
+
+            result.Name.Should().Be("Barry Jenkins");
+
+            try
+            {
+                result = repo.SingleOrDefault(x => x.FirstName != null);
+            }
+            catch (Exception ex)
+            {
+                ex.GetType().Should().Be(typeof(InvalidOperationException));
+            }
+        }
+
+        private CustomerRepository getRepo() => getRepo(this.mapper);
+
+        private CustomerRepository getRepo(IMapper mapper) => new CustomerRepository(new Implementations.Context.DbContext(), mapper);
     }
 }
--- src/MappingRepository/MappingRepository.cs
+++ src/MappingRepository/MappingRepository.cs
@@ -8,17 +8,48 @@ using System.Linq.Expressions;
 
 namespace MappingRepository
 {
+    /// <summary>
+    /// A MappingRepository instance represents a repository that automatically maps incoming and
+    /// outgoing objects to and from your desired types. A classic example of use would be using
+    /// repositories derived from <see cref="MappingRepository{TEntity, TDestination, TKey}"/> to
+    /// create abstraction, allowing you to implement DDD.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the underlying database entity.</typeparam>
+    /// <typeparam name="TDestination">The type of your abstracted domain layer object.</typeparam>
+    /// <typeparam name="TKey">The type of the common key that your entities share.</typeparam>
     public abstract class MappingRepository<TEntity, TDestination, TKey>
         where TEntity : class, IMappingRepositoryEntity<TKey>, new()
         where TDestination : IMappingRepositoryDestination<TKey>
         where TKey : IEquatable<TKey>
     {
-        public MappingRepository(DbContext dbContext, IMapper mapper)
+        private DbContext dbContext;
+
+        private IMapper mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingRepository{TEntity, TDestination,
+        /// TKey}"/> class. Call this constructor when deriving from <see
+        /// cref="MappingRepository{TEntity, TDestination, TKey}"/>.
+        /// </summary>
+        /// <param name="dbContext">The Entity Framework context to use for database access.</param>
+        /// <param name="mapper">
+        /// An AutoMapper IMapper instance, containing the maps and configuration for mapping operations.
+        /// </param>
+        protected MappingRepository(DbContext dbContext, IMapper mapper)
         {
             this.dbContext = dbContext;
             this.mapper = mapper;
         }
 
+        private DbSet<TEntity> dbSet => this.dbContext.Set<TEntity>();
+
+        private IConfigurationProvider mapperConfig => this.mapper.ConfigurationProvider;
+
+        /// <summary>
+        /// Adds the given entity into context.
+        /// </summary>
+        /// <param name="obj">The entity to add.</param>
+        /// <returns>Returns the newly-generated ID of the inserted entity.</returns>
         public virtual TKey Add(TDestination obj)
         {
             var entity = this.mapper.Map<TEntity>(obj);
@@ -30,6 +61,11 @@ namespace MappingRepository
             return result.Id;
         }
 
+        /// <summary>
+        /// Adds the given collection of entities into context.
+        /// </summary>
+        /// <param name="objs">The collection of entities to add.</param>
+        /// <returns>The number of successfully inserted entities.</returns>
         public virtual int AddRange(IEnumerable<TDestination> objs)
         {
             var entities = this.mapper.Map<IEnumerable<TEntity>>(objs);
@@ -39,11 +75,24 @@ namespace MappingRepository
             return this.dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Determines whether any element of a sequence satisfies a condition.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        /// true if any elements in the source sequence pass the test in the specified predicate;
+        /// otherwise, false.
+        /// </returns>
         public virtual bool Any(Expression<Func<TEntity, bool>> predicate)
         {
             return this.dbSet.Any(predicate);
         }
 
+        /// <summary>
+        /// Removes the specified entity from the context.
+        /// </summary>
+        /// <param name="id">The unique ID of the entity to delete.</param>
+        /// <returns>The number of deleted entities.</returns>
         public virtual int Delete(TKey id)
         {
             var entity = new TEntity()
@@ -56,6 +105,11 @@ namespace MappingRepository
             return this.dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Removes the given collection of entities from the context.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>The number of deleted entities.</returns>
         public virtual int DeleteRange(Expression<Func<TEntity, bool>> predicate)
         {
             this.dbSet.RemoveRange(this.dbSet.Where(predicate));
@@ -72,11 +126,29 @@ namespace MappingRepository
             return this.dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Returns all elements of a sequence that satisfy a specified condition or an empty
+        /// collection if no elements satisfy the condition.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        /// An empty collection if no element passes the test specified by predicate; otherwise, all
+        /// elements in source that pass the test specified by predicate.
+        /// </returns>
         public IList<TDestination> FindBy(Expression<Func<TEntity, bool>> predicate)
         {
             return this.dbSet.AsNoTracking().Where(predicate).ProjectToList<TDestination>(this.mapperConfig);
         }
 
+        /// <summary>
+        /// Returns the first element of a sequence that satisfies a specified condition or a
+        /// default value if no such element is found.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        /// default(TDestination) if source is empty or if no element passes the test specified by
+        /// predicate; otherwise, the first element in source that passes the test specified by predicate.
+        /// </returns>
         public TDestination FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
         {
             return this.dbSet.AsNoTracking().Where(predicate).ProjectToFirstOrDefault<TDestination>(this.mapperConfig);
@@ -87,11 +159,25 @@ namespace MappingRepository
             return this.dbSet.AsNoTracking().Where(x => x.Id.Equals(id)).ProjectToSingleOrDefault<TDestination>(this.mapperConfig);
         }
 
+        /// <summary>
+        /// Returns the only element of a sequence that satisfies a specified condition or a default
+        /// value if no such element exists; this method throws an exception if more than one
+        /// element satisfies the condition.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        /// The single element of the input sequence that satisfies the condition in predicate, or
+        /// default(TDestination) if no such element is found.
+        /// </returns>
         public TDestination SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
         {
             return this.dbSet.AsNoTracking().Where(predicate).ProjectToSingleOrDefault<TDestination>(this.mapperConfig);
         }
 
+        /// <summary>
+        /// Presents a <see cref="IQueryable{TDestination}"/> object for consumption in your derived repository. 
+        /// </summary>
+        /// <returns></returns>
         protected IQueryable<TDestination> AsQueryable()
         {
             return this.dbSet.AsNoTracking().ProjectToQueryable<TDestination>(this.mapperConfig);
@@ -106,12 +192,5 @@ namespace MappingRepository
         {
             return this.dbSet.AsNoTracking().ProjectToQueryable<TCustomType>(this.mapperConfig);
         }
-
-        private DbContext dbContext;
-        private IMapper mapper;
-
-        private DbSet<TEntity> dbSet => this.dbContext.Set<TEntity>();
-
-        private IConfigurationProvider mapperConfig => this.mapper.ConfigurationProvider;
     }
 }
